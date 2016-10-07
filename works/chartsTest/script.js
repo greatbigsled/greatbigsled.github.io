a2b6a8a4d0b529494dd4260cee4f301ed815fd88
greatbigsled <vahabond@ya.ru>
2016-10-07 10:05:11 +0300

    some

--- works/chartsTest/script.js
+++ works/chartsTest/script.js
@@ -1,6 +1,6 @@
 let canvas  = document.getElementById('graph-canvas');
 let ctx     = canvas.getContext('2d');
-
+let SEXY_COLORS = ['#69D2E7', '#A7DBD8', '#E0E4CC', '#F38630', '#FE4365'];
 
 let scatterData = [
         {x: 10, y: 10, r: 37},
@@ -9,9 +9,23 @@ let scatterData = [
         {x: 40, y: 5.7, r: 11},
         {x: 32, y: 11, r: 28}
 ];
-
-
-
+let lineData = {
+    1: [1, 12, 3],
+    4: [12, 31, 4],
+    5: [3, 24, 4],
+    6: [12, 17, 6],
+    12: [8, 2, 9],
+    13: [21, 34, 6],
+    21: [6, 7, 8]
+};
+let stackedBarData = [
+    ['jan', 1, 4, 5, 6],
+    ['jan', 2, 3, 5, 6],
+    ['jan', 3, 10, 11],
+    ['mar', 4, 9, 7, 10],
+    ['may', 3, 10, 8, 1],
+    ['sep', 11, 1, 9, 0]
+];
 
 
 
@@ -31,16 +45,18 @@ c.drawLine = function drawLine(ctx, x0, y0, x1, y1, {color='#000', width=1}){
     ctx.strokeStyle = color;
     ctx.lineWidth = width;
     ctx.stroke();
-};
-c.drawRect = function drawRect(ctx, x, y, w, h){
+}
+
+c.drawRect = function drawRect(ctx, x, y, w, h, {fillStyle='#bacaba', strokeWidth=2, strokeStyle='515851'}){
     ctx.beginPath();
     ctx.rect(x, y, w, h);
-    ctx.fillStyle = '#bacaba';
+    ctx.fillStyle = fillStyle;
     ctx.fill();
-    ctx.strokeStyle = '#515851';
-    ctx.strokeWidth = 2;
+    ctx.strokeStyle = strokeStyle;
+    ctx.strokeWidth = strokeWidth;
     ctx.stroke();
-};
+}
+
 c.drawTxt = function drawTxt( ctx, text, x, y, { baseline='middle', align='center', color='#000', size='16px', width=1 } ){
     ctx.strokeStyle = color;
     ctx.font = size + 'sans-serif';
@@ -50,6 +66,22 @@ c.drawTxt = function drawTxt( ctx, text, x, y, { baseline='middle', align='cente
     ctx.strokeText(text, x, y);
 }
 
+c.drawCircle = function drawCircle(ctx, {x, y, r, fillStyle='green', strokeStyle='#000', lineWidth='1', hasCenter=false}){
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2*Math.PI, false);
+    ctx.fillStyle = fillStyle;
+    ctx.fill();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.stroke();
+
+    if (hasCenter){
+        ctx.beginPath();
+        ctx.arc(x, y, 1, 0, 2*Math.PI, false);
+        ctx.fillStyle = '#000';
+        ctx.fill();
+    }
+}
 
 
 
@@ -142,17 +174,17 @@ function hist(arr, canvas){
 }
 
 
-function drawAxis(xVals, yVals){
+function drawAxis(xVals, yVals, {xTxt, yTxt}){
     let cellW = canvasOpts.pageW/xVals;
     let cellH = canvasOpts.pageH/yVals;
 
     for (let x = 0; x <= xVals; x++) {
-        c.drawTxt(ctx, x, x*cellW+canvasOpts.padding, canvasOpts.pageH+canvasOpts.padding+5, {baseline:'top', align: 'left', size: '14px', width: .2});
+        c.drawTxt(ctx, xTxt ? xTxt[x] : x, x*cellW+canvasOpts.padding, canvasOpts.pageH+canvasOpts.padding+5, {baseline:'top', align: 'left', size: '14px', width: .2});
     }
 
     for (let y = 0; y <= yVals; y++) {
     // for (let y = yVals; y >= 0; y--) {
-        c.drawTxt(ctx, yVals-y, canvasOpts.padding-5, y*cellH+canvasOpts.padding, {baseline:'bottom', align: 'right', size: '14px', width: .2});
+        c.drawTxt(ctx, yTxt ? yTxt[yVals-y] : yVals-y, canvasOpts.padding-5, y*cellH+canvasOpts.padding, {baseline:'bottom', align: 'right', size: '14px', width: .2});
     }
 }
 
@@ -180,6 +212,16 @@ function drawGrid(xSize, ySize){
     }
 }
 
+
+function drawScatterElem(ctx, data, {fillStyle= 'green', lineWidth=1, strokeStyle='#000'}){
+    ctx.beginPath();
+    ctx.arc(data.x, data.y, data.r, 0, 2*Math.PI, false);
+    ctx.fillStyle = fillStyle;
+    ctx.fill();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = strokeStyle;
+    ctx.stroke();
+}
 function scatter(data){
 
     let dataMaxX = 0,
@@ -195,21 +237,96 @@ function scatter(data){
             dataMaxY = obj.y;
     });
 
+
+    let stepX = canvasOpts.pageW/dataMaxX,
+        stepY = canvasOpts.pageH/dataMaxY;
+
+    let padding = canvasOpts.padding;
+
     drawGrid(dataMaxX, dataMaxY);
-    drawAxis(dataMaxX, dataMaxY);
+    drawAxis(dataMaxX, dataMaxY, {});
+
+
+    data.map(val=>{
+
+        let itemData = {
+            x: val.x*stepX + padding,
+            y: (canvasOpts.pageH - val.y*stepY)+padding,
+            r: val.r,
+            hasCenter: true
+        }
+        c.drawCircle(ctx, itemData);
+    });
 
 }
 
 scatter(scatterData);
 
-// drawGrid(4,3);
+
+function drawLineChart(data){
+    let dataMaxX = 0,
+        dataMaxY = 0;
+
+    let padding = canvasOpts.padding;
+
+    for (let val in data){
+        if (val > dataMaxX){
+            dataMaxX = val;
+        }
+
+        let mx = Math.max.apply(null, data[val]);
+
+        if (mx > dataMaxY){
+            dataMaxY = mx;
+        }
+    }
+
+    let stepX = canvasOpts.pageW/dataMaxX,
+        stepY = canvasOpts.pageH/dataMaxY;
+
+    drawGrid(dataMaxX, dataMaxY);
+    drawAxis(dataMaxX, dataMaxY);
+
+}
+
+// drawLineChart(lineData);
+
+function drawStackedBar(data){
+    let env = {};
+    env.mx = 0,
+    env.my = 0;
+
+    env.my = data.length;
+
+    data.map(val=>{
+        let mx = Math.max.apply(null, val.slice(1));
+        if (mx > env.mx){
+            env.mx = mx;
+        }
+    });
+
+    env.x = canvasOpts.pageW/env.mx,
+    env.y = canvasOpts.pageH/env.my;
+
+    let padding = canvasOpts.padding;
+
+    let yTxt = [];
+    data.map(val=> yTxt.push(val[0]));
+
+    drawGrid(env.mx, env.my);
+    drawAxis(env.mx, env.my, {yTxt});
+
+    for (var i = 0; i < data.length; i++) {
+        let chunk = data[i];
+        let valueStart = 0;
+        chunk.slice(0).map((val, idx) =>{
+            c.drawRect(ctx, env.x*valueStart+padding, canvasOpts.realPageH - (env.y*i+ padding), env.x*(val) )
+        })
+    }
+}
 
 
-
-
-
-
-
+// drawStackedBar(stackedBarData);
 
 
 
